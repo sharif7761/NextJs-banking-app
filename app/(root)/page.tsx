54a544b17d6ea08dd7faec98c554d8698e8044bb
sharif-ahmed <sharif7761@gmail.com>
2024-09-01 10:54:23 +0600

    project setup and routing

---
+++ app/(root)/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import HeaderBox from "@/components/components/HeaderBox";
+import TotalBalanceBox from "@/components/components/TotalBalanceBox";
+
+const Home = () => {
+    const loggedIn = { firstName: 'Sharif', lastName: 'JSM', email: 'contact@jsmastery.pro' };
+
+    return (
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+            </div>
+        </section>
+    )
+}
+export default Home
